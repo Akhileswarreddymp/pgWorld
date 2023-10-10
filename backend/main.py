52a66d5037989bcc76c6657c3d27bf09bb2faa16
M P Akhileswar Reddy <129936996+Akhileswarreddymp@users.noreply.github.com>
2023-10-10 15:31:14 +0400

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -44,10 +44,10 @@ async def redis_store(otp):
 
 
 @app.post("/register", tags=['Authentication'])
-async def register(username,password):
+async def register(data: verify_params):
     redis_client = redisclient()
-    mail = redis_client.redis_client.setex("email", 300, username)
-    hash_temp_password = hashlib.md5(password.encode('utf-8')).hexdigest()
+    mail = redis_client.redis_client.setex("email", 300, data.username)
+    hash_temp_password = hashlib.md5(data.password.encode('utf-8')).hexdigest()
     passwor = redis_client.redis_client.setex("password", 300, hash_temp_password)
     print("email===>", redis_client.redis_client.get("email").decode())
     print("passwor===>",redis_client.redis_client.get("password").decode())
@@ -55,12 +55,12 @@ async def register(username,password):
 
 
 @app.post("/login", tags=['Authentication'])
-def verification(username,password):
+def verification(request: verify_params):
     redis_client = redisclient()
-    print("verification email==>",username)
+    print("verification email==>",request.username)
     print("verification password===>",verification)
-    passwo = hashlib.md5(password.encode('utf-8')).hexdigest()
-    if redis_client.redis_client.get("email").decode() == username and redis_client.redis_client.get("password").decode() == passwo:
+    passwo = hashlib.md5(request.password.encode('utf-8')).hexdigest()
+    if redis_client.redis_client.get("email").decode() == request.username and redis_client.redis_client.get("password").decode() == passwo:
         return {"msg" : "Successfully Logged in "}
     else:
         raise HTTPException(status_code=401, detail="Wrong Credentials received")
